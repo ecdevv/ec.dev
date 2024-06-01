36d41bf4b535a0bf1152346301819472e82e62f5
Ech2k20 <ech2447@gmail.com>
2024-06-01 12:48:28 -0500

    visual edits again

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -49,7 +49,7 @@ const Navbar = () => {
     <header>
       <nav className = "fixed top-0 left-0 bg-main w-full h-[5rem] shadow-md z-[10]">
         {/* Desktop Menu */}
-        <div className = "flex w-full h-full max-w-[85%] justify-between items-center p-2 mx-auto">
+        <div className = "flex w-full h-full max-w-[82.5%] justify-between items-center p-2 mx-auto">
           <Link href = "#home" aria-label = "Home" className = "flex gap-x-1 items-end">
             <img
               src = "/favicon/android-chrome-256x256.png"
@@ -68,7 +68,7 @@ const Navbar = () => {
               <li><Link href = "#contact" aria-label = "Contact" className = "font-openSans font-bold text-lg hover:text-[hsl(212,100%,53%)] transition-all ease-in-out duration-[200ms]">Contact</Link></li>
             </ul>
   
-            <div className = "flex lg:justify-start justify-center gap-x-4">
+            {/* <div className = "flex lg:justify-start justify-center gap-x-4">
               <Link href = "https://www.linkedin.com/" aria-label = "LinkedIn" target = "_blank" rel = "noreferrer">
                 <svg 
                   xmlns = "http://www.w3.org/2000/svg" 
@@ -89,9 +89,9 @@ const Navbar = () => {
                   <path fillRule = "evenodd" clipRule = "evenodd" d = "M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"/>
                 </svg>
               </Link>
-            </div>
+            </div> */}
 
-            <button onClick = {toggleTheme} className = "hidden absolute right-[2.5%] xl:flex justify-center items-center">
+            <button onClick = {toggleTheme} className = "hidden absolute right-[3%] xl:flex justify-center items-center">
               <svg
                 viewBox = "0 0 24 24"
                 fill = "currentColor"
@@ -117,7 +117,7 @@ const Navbar = () => {
           unmountOnExit
         >
           {/* <div className = {`absolute flex flex-col top-0 portrait:w-[66%] landscape:w-screen h-screen bg-navbarMobileColor gap-[2vh] transition-all ease-in-out duration-300 ${menuOpen ? 'right-0' : 'portrait:right-[-66%] landscape:right-[-100%]'}`}>*/}
-          <div className = "absolute flex flex-col top-0 right-0 portrait:w-[66%] landscape:w-screen h-screen bg-navbarMobileColor gap-[2vh]">
+          <div className = "xl:hidden absolute flex flex-col top-0 right-0 portrait:w-[66%] landscape:w-screen h-screen bg-navbarMobileColor gap-[2vh]">
             <div className = "absolute top-0 right-0 p-[3vh] landscape:p-[6vh]">
               <button onClick = {handleMobileMenu}>
                 <VscClose className = "sm:w-[36px] w-[32px] h-auto"/>
